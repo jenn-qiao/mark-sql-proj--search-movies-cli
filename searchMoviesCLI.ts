9f54ee0de1beaa2f4161943f14f58fbe07598a2f
Jennifer Qiao <qiaojennifer97@gmail.com>
2022-09-03 02:00:16 +0100

    version 1

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -4,5 +4,35 @@ import { Client } from "pg";
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-const client = new Client({ database: 'omdb' });
-console.log("Welcome to search-movies-cli!");
+
+
+async function searchMovie() {
+    var readlineSync = require('readline-sync');
+    const client = new Client({ database: 'omdb' });
+    await client.connect();
+    console.log("Welcome to search-movies-cli!");
+    let searchTerm = readlineSync.question('search for a movie (or press q to quit) ')
+    try{
+        while (searchTerm !== 'q') {
+        const text = "SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count FROM movies WHERE LOWER(name) like LOWER($1) AND kind = 'movie' ORDER BY date DESC LIMIT 10"
+        const values = [`%${searchTerm}%`]
+
+        const res = await client.query(text,values);
+        console.table(res.rows);
+        searchTerm = readlineSync.question('search another movie (or press q to quit) ')
+    }
+    
+}
+catch(err) {
+    console.log(`${err}`)
+}
+finally{
+    await client.end();
+console.log("Client disconnected successfully")
+}
+}
+
+searchMovie()
+// while (searchString !== 'q')
+// await client.end();
+
